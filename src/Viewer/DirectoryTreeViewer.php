afebd6b61946e212aa90f75e4f76d2c9c4d76383
Harper Jang <harper.jang@outlook.com>
2025-05-12 22:36:52 +0800

    refactor(viewer): improve error message
    Refined the thrown exception message to list acceptable integer
    constants, aiding in debugging incorrect format inputs.

--- src/Viewer/DirectoryTreeViewer.php
+++ src/Viewer/DirectoryTreeViewer.php
@@ -31,7 +31,9 @@ class DirectoryTreeViewer
             self::FORMAT_TEXT_INDENTED => $transformer->toTextIndented($tree),
             self::FORMAT_MARKDOWN_LIST => $transformer->toMarkdownList($tree),
             self::FORMAT_HTML_LIST     => $transformer->toHtmlList($tree),
-            default                    => throw new DirectoryTreeException("Invalid format: {$format}. Use FORMAT_* constants"),
+            default                    => throw new DirectoryTreeException(
+                "Invalid format: {$format}. Allowed values: 1 (TEXT_TREE), 2 (TEXT_INDENTED), 3 (MARKDOWN_LIST), 4 (HTML_LIST)",
+            ),
         };
     }
 }
